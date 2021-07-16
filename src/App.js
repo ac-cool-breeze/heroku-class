99f287b6f4f7fa7327d175c299e0dea3ce936068
ac-cool-breeze <alexcolter@gmail.com>
2021-07-16 11:12:13 -0500

    first try

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const isAuth = {
   isAuthenticated: false,
 }
 
-let route = <Route path="/login" component={login}><Login /></Route>
+let route = <Route path="/login" component={Login}><Login /></Route>
 
 function App() {
 
